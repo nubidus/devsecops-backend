5f493449e5cf4fe2eee923d2de173f446da7ce76
lubidap <idilmete@gmail.com>
2023-08-19 14:27:50 +0300

    kusrapplicationtest

---
+++ src/test/java/com/nsahin/kurs/KursApplicationTest.java
@@ -0,0 +1,20 @@
+package com.nsahin.kurs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class KursApplicationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
